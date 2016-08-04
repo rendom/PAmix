7dae5e890a6bd398a7ddd00b857b450df38e0b7c
Rendom <a@vvt.nu>
2016-08-04 23:57:37 +0200

    Colored meters

--- src/pamix.cpp
+++ src/pamix.cpp
@@ -28,7 +28,8 @@ struct UpdateData
 
 #define DECAY_STEP 0.04
 #define MAX_VOL 1.5
-#define SYM_VOLBAR L'\u25ae' //▮
+#define SYM_VOLBAR L"\u25ae" //▮
+#define SYM_SPACE L"\u0020" // White space
 
 // GLOBAL VARIABLES
 bool     running         = true;
@@ -61,11 +62,34 @@ void generateMeter(int y, int x, int width, const double pct, const double maxvo
 		return;
 	int filled = pct / maxvol * (double)segments;
 
-	std::wstring meter = L"[]";
+    mvaddwstr(y, x, L"[");
+    x++;
 
-	meter.insert(1, filled, SYM_VOLBAR);
-	meter.insert(filled + 1, segments - filled, ' ');
-	mvaddwstr(y, x, meter.c_str());
+    for(int i=0;i<filled;i++){
+        float p = (float) i / (float) (maxvol*100);
+
+        int colorPair
+        if(p<.333f){
+            colorPair = COLOR_PAIR(1);
+        } elseif(p>.333f && p<.666f){
+            colorPair = COLOR_PAIR(2);
+        }else{
+            colorPair = COLOR_PAIR(3);
+        }
+
+        attron(colorPair);
+        mvaddwstr(y, x, SYM_VOLBAR);
+        attroff(colorPair);
+        x++;
+    }
+
+    mvaddwstr(y, x, L"]");
+    x++;
+
+    for(int i=0;i<(segments-filled);i++){
+        mvaddwstr(y, x, SYM_SPACE);
+        x++;
+    }
 }
 
 void updatesinks(PAInterface *interface)
@@ -328,10 +352,21 @@ void sig_handle_resize(int s)
 	signal_update(true);
 }
 
+void initColors()
+{
+    start_color();
+    init_pair(1, COLOR_GREEN, 0);
+    init_pair(2, COLOR_YELLOW, 0);
+    init_pair(3, COLOR_RED, 0);
+}
+
 int main(int argc, char **argv)
 {
 	setlocale(LC_ALL, "");
 	initscr();
+
+    initColors();
+
 	curs_set(0); // make cursor invisible
 	noecho();
 
